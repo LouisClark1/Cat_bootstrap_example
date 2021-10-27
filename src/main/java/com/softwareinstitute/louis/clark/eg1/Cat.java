25779539a0898d4ffe35a9848862c2b808fe112d
Louis Clark <lclark@softwareinstitute>
2021-10-27 15:22:56 +0100

    first push

---
+++ src/main/java/com/softwareinstitute/louis/clark/eg1/Cat.java
@@ -0,0 +1,10 @@
+package com.softwareinstitute.louis.clark.eg1;
+
+public class Cat {
+    private String name;
+    private int age;
+    private String breed;
+    public Cat(String name, int age, String breed){
+
+    }
+}
