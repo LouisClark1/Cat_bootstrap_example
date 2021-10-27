2b8b7267b866955f3ad289bb672eedb859ac687f
Louis Clark <lclark@softwareinstitute>
2021-10-27 16:22:17 +0100

    most of the code is there but it isn't running yet

--- src/main/java/com/softwareinstitute/louis/clark/eg1/Cat.java
+++ src/main/java/com/softwareinstitute/louis/clark/eg1/Cat.java
@@ -4,7 +4,47 @@ public class Cat {
     private String name;
     private int age;
     private String breed;
-    public Cat(String name, int age, String breed){
 
+    public Cat(String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
     }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(){
+        this.name = name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+    public void setAge(){
+        this.age = age;
+    }
+
+    public String getBreed(){
+        return breed;
+    }
+    public void setBreed(){
+        this.breed = breed;
+    }
+
+
+
+    public String getJsonObjectCat() {
+        return "{" +
+
+                "\"name\"= \"" + name +
+
+                "\", \"age\"=\"" + age +
+
+                "\", \"breed\"=\"" + breed +
+
+                "\"}";
+    }
+
+
 }
--- src/main/java/com/softwareinstitute/louis/clark/eg1/Eg1Application.java
+++ src/main/java/com/softwareinstitute/louis/clark/eg1/Eg1Application.java
@@ -2,12 +2,19 @@ package com.softwareinstitute.louis.clark.eg1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController // i dont know why we need this
 public class Eg1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Eg1Application.class, args);
 	}
-
+	@GetMapping("/getcat")
+	public String catResponse(){
+		Cat cat = new Cat("billy", 7, "Dirty");
+	return cat.getJsonObjectCat();
+	}
 }
